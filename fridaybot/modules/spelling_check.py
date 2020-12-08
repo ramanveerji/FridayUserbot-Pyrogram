3c6045504d8e6ed6ecb85419bab832d0e4df7959
StarkGang <StarkGangZ@gmail.com>
2020-12-08 19:58:11 +0530

    :)

---
+++ fridaybot/modules/spelling_check.py
@@ -0,0 +1,19 @@
+from textblob import TextBlob
+from fridaybot.Configs import Config
+from fridaybot import CMD_HELP
+
+from fridaybot.utils import admin_cmd
+
+@friday.on(admin_cmd(pattern="(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if Config.AUTO_SPELL_FIX != True:
+        return
+    input_str = event.pattern_match.group(1)
+    if input_str.startwith(".", "!", "'", "/", ":", "*"):
+        pass
+    else:
+        bm = TextBlob(input_str)
+        await event.edit(bm)
+      
