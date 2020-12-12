aa863e13b74e1ca3ce24a3696004aafc9daf6eae
Starkgangs <Starkgangz@gmail.com>
2020-12-12 14:24:11 +0000

    pylint: auto fixes

--- fridaybot/modules/dictionary.py
+++ fridaybot/modules/dictionary.py
@@ -1,8 +1,7 @@
 """Syntax: .meaning <word>"""
 
-import requests
-from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
 from PyDictionary import PyDictionary
+from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
 
 from fridaybot import CMD_HELP
 
@@ -14,13 +13,16 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    dictionary=PyDictionary()
-    a = (dictionary.meaning(input_str))
+    dictionary = PyDictionary()
+    a = dictionary.meaning(input_str)
     b = a.get("Noun")
     chsaiujwal = ""
     for x in b:
-	    chsaiujwal += x+"\n"
-    await stark.edit(f"<b> meaning of {input_str} is:-</b>\n {chsaiujwal}", parse_mode="HTML",)
+        chsaiujwal += x + "\n"
+    await stark.edit(
+        f"<b> meaning of {input_str} is:-</b>\n {chsaiujwal}",
+        parse_mode="HTML",
+    )
 
 
 CMD_HELP.update(
