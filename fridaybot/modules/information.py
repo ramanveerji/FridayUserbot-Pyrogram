e8920cbc193949ad358577601bf7809d74babf33
StarkGang <StarkGangZ@gmail.com>
2020-12-13 11:54:05 +0530

    Final Fix // 5.0

--- fridaybot/modules/information.py
+++ fridaybot/modules/information.py
@@ -146,9 +146,9 @@ async def gibinfo(event):
         )
     else:
         try:
-            user = await borg.get_entity(event.pattern_match.group(1))
+            user = await friday.get_entity(event.pattern_match.group(1))
         except ValueError:
-            await event.edit("<i>No User Found.</i>")
+            await event.edit("<i>No User Found.</i>", parse_mode='HTML')
             return
     try:
         cas_url = f"https://combot.org/api/cas/check?user_id={user.id}"
@@ -162,16 +162,16 @@ async def gibinfo(event):
         reason = f"<i>False</i>"
     hmmyes = sclient.is_banned(user.id)
     if hmmyes:
-        oki = f"""<i>True</i> 
-<b>Reason :</b> <i>{hmmyes.reason}</i>"""
+        oki = f"""<i>True</i>
+<b>~ Reason :</b> <i>{hmmyes.reason}</i>"""
     else:
         oki = "<i>False</i>"
     infomsg = (
-        f"<b>Info Of</b> <a href=tg://user?id={user.id}>{user.first_name}</a>:"
-        f"<b>- Username:</b> <i>{user.username}</i>\n"
-        f"<b>- ID:</b> <i>{user.id}</i>\n"
-        f"<b>- Bot:</b> <i>{user.bot}</i>\n"
-        f"<b>- CAS Banned :</b> {reason}"
+        f"<b>Info Of</b> <a href=tg://user?id={user.id}>{user.first_name}</a>: \n"
+        f"<b>- Username :</b> <i>{user.username}</i>\n"
+        f"<b>- ID :</b> <i>{user.id}</i>\n"
+        f"<b>- Bot :</b> <i>{user.bot}</i>\n"
+        f"<b>- CAS Banned :</b> {reason} \n"
         f"<b>- AntispamInc Banned :</b> {oki}"
     )
     await event.edit(infomsg, parse_mode="HTML")
