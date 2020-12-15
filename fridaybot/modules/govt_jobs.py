6b443dd32ec78c0bbb8f67b48dcad4744457bf79
Starkgangs <Starkgangz@gmail.com>
2020-12-15 06:11:53 +0000

    pylint: auto fixes

--- fridaybot/modules/govt_jobs.py
+++ fridaybot/modules/govt_jobs.py
@@ -1,40 +1,33 @@
-import requests 
-import asyncio
-import math
-import os
-import heroku3
 import requests
+from bs4 import BeautifulSoup
+
 from fridaybot import CMD_HELP
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
-import pandas as pd 
-from bs4 import BeautifulSoup 
-from asyncio import wait
-from telethon import events
 
 
 @friday.on(friday_on_cmd(pattern=r"govtjob"))
 @friday.on(sudo_cmd(pattern="govtjob", allow_sudo=True))
 async def _(givejob):
 
-    res = '' 
-    def getdata(url): 
-      r = requests.get(url) 
-      return r.text 
+    res = ""
+
+    def getdata(url):
+        r = requests.get(url)
+        return r.text
+
     await edit_or_reply(givejob, "Trying To Fetch Jobs...")
 
-    htmldata = getdata("https://www.sarkariresult.com/latestjob.php") 
+    htmldata = getdata("https://www.sarkariresult.com/latestjob.php")
 
-    soup = BeautifulSoup(htmldata, 'html.parser') 
+    soup = BeautifulSoup(htmldata, "html.parser")
 
     for li in soup.find_all("div", id="post"):
-      res += (li.get_text()) 
-    
-    
+        res += li.get_text()
+
     lmao = "Information About Government Jobs Gathered Successfully\n\n" + str(res)
-    
+
     with open("jobs.txt", "w") as job:
-      job.write(lmao)
-    kk = lmao
+        job.write(lmao)
     await givejob.client.send_file(
         givejob.chat_id,
         "jobs.txt",
@@ -49,7 +42,3 @@ CMD_HELP.update(
 \n**Usage :** Gathers information about all the open government jobs."
     }
 )
-
-
-
-
