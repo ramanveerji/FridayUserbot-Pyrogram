bf22ff31a137ef819f69a00ba05f9da78c36951c
StarkGang <StarkGangZ@gmail.com>
2020-12-15 10:39:44 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -1,1 +1,203 @@
+import os
+import asyncio
+import json
+import math
+import os
+import subprocess
+import asyncio
+import os
+import re
+import shlex
+from os.path import basename
+from typing import List, Optional, Tuple
+import time
+from pymediainfo import MediaInfo
+from telethon.tl.types import MessageMediaPhoto
+sedpath = Config.TMP_DOWNLOAD_DIRECTORY
+from fridaybot import logging
 
+logger = logging.getLogger("[WARNING]")
+if not os.path.isdir(sedpath):
+    os.makedirs(sedpath)
+
+# Thanks To Userge-X
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    """Generic progress_callback for uploads and downloads."""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current != total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "[{0}{1}] {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            await event.edit(
+                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("{}\n{}".format(type_of_ps, tmp))
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
+  
+# Thanks To Userge-X
+# Ported By @STARKXD
+async def convert_to_image(event):
+    lmao = await event.get_reply_message()
+    if not (
+            lmao.gif
+            or lmao.audio
+            or lmao.voice
+            or lmao.video
+            or lmao.video_note
+            or lmao.sticker
+
+    ):
+        await event.edit('`Format Not Supported.`')
+        return
+    else:
+        try:
+            c_time = time.time()
+            downloaded_file_name = await borg.download_file(
+                lmao.media,
+                sedpath,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, event, c_time, "`Downloading...`")
+                ),
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await event.edit(str(e))
+        else:
+            await event.edit(
+                "Downloaded to `{}` successfully !!".format(downloaded_file_name)
+            )
+    if not os.path.exists(downloaded_file_name):
+        await event.edit('Download Unsucessfull :(')
+        return
+    elif lmao.sticker and lmao.sticker.mime_type == 'application/x-tgsticker':
+            pathofsticker = downloaded_file_name
+            image_name = sedpath + 'image.png'
+            os.system(f"lottie_convert.py --frame 0 -if lottie -of png {pathofsticker} {image_name}")
+            os.remove(pathofsticker)
+            if not os.path.exists(image_name):
+                await event.edit("`Wasn't Able To Fetch Shot.`")
+                return
+            lmao_final = image_name
+    elif lmao.sticker and lmao.sticker.mime_type == 'image/webp':
+            pathofsticker2 = downloaded_file_name
+            image_new_path = sedpath + 'image.png'
+            os.rename(pathofsticker2, image_new_path)
+            if not os.path.exists(image_new_path):
+                await event.edit("`Wasn't Able To Fetch Shot.`")
+                return
+            lmao_final = image_new_path
+            os.remove(pathofsticker2)
+    elif lmao.audio:
+            sed_p = downloaded_file_name
+            hmmyes = sedpath + 'stark.mp3'
+            imgpath = sedpath + 'stark.jpg'
+            os.rename(sed_p, hmmyes)
+            os.system(f"ffmpeg -i {hmmyes} -filter:v scale=500:500 -an {imgpath}")
+            if not os.path.exists(imgpath):
+                await event.edit("`Wasn't Able To Fetch Shot.`")
+                return
+            lmao_final = imgpath
+            os.remove(sed_p)
+    elif lmao.gif or lmao.video or lmao.video_note:
+            sed_p2 = downloaded_file_name
+            jpg_file = os.path.join(sedpath, "image.jpg")
+            await take_screen_shot(sed_p2, 0, jpg_file)
+            if not os.path.exists(jpg_file):
+                await event.edit("`Couldn't Fetch. SS`")
+                return
+            lmao_final = jpg_file
+    await event.edit('`Almost Completed.`')
+    return lmao_final
+# Thanks To Userge-X
+async def crop_vid(input_vid: str, final_path: str):
+    media_info = MediaInfo.parse(input_vid)
+    for track in media_info.tracks:
+        if track.track_type == "Video":
+            aspect_ratio = track.display_aspect_ratio
+            height = track.height
+            width = track.width
+    if aspect_ratio != 1:
+        crop_by = width if (height > width) else height
+        os.system(
+            f'ffmpeg -i {input_vid} -vf "crop={crop_by}:{crop_by}" {final_path}'
+        )
+        os.remove(input_vid)
+    else:
+        os.rename(input_vid, final_path)
+# Thanks To Userge-X
+async def take_screen_shot(
+    video_file: str, duration: int, path: str = ""
+) -> Optional[str]:
+    """ take a screenshot """
+    _logger.info(
+        "[[[Extracting a frame from %s ||| Video duration => %s]]]",
+        video_file,
+        duration,
+    )
+    ttl = duration // 2
+    thumb_image_path = path or os.path.join(
+        sedpath, f"{basename(video_file)}.jpg"
+    )
+    command = f'''ffmpeg -ss {ttl} -i "{video_file}" -vframes 1 "{thumb_image_path}"'''
+    err = (await runcmd(command))[1]
+    if err:
+        logger.error(err)
+    return thumb_image_path if os.path.exists(thumb_image_path) else None
