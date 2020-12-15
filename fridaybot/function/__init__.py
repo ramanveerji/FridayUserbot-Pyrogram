815263a21b14bdd6247b42f5ae9619fe516b9bbb
StarkGang <StarkGangZ@gmail.com>
2020-12-15 21:15:34 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -8,7 +8,6 @@ import subprocess
 import time
 from os.path import basename
 from typing import List, Optional, Tuple
-
 from pymediainfo import MediaInfo
 from telethon.tl.types import MessageMediaPhoto
 
