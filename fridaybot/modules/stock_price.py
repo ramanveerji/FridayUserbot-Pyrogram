40f3179c2029e53fd97a082ccef3ce99ae6d9631
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-12-16 09:50:04 +0530

    New Plugin. Stock_price.py 🐕🐕🐕

---
+++ fridaybot/modules/stock_price.py
@@ -0,0 +1,36 @@
+from yahoo_fin import stock_info as si
+
+from uniborg.util import friday_on_cmd
+
+from fridaybot import CMD_HELP
+from fridaybot.utils import admin_cmd
+import requests
+
+
+@friday.on(admin_cmd(pattern="liveprice (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    try:
+      a = si.get_live_price(input_str)
+
+
+      def get_symbol(symbol):
+        url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+        result = requests.get(url).json()
+        for x in result['ResultSet']['Result']:
+          if x['symbol'] == symbol:
+            return x['name']
+
+      lmao = input_str.upper()
+      company = get_symbol(lmao)
+      if company==None:
+        await event.edit(
+          f"<b><u>Stock Information Gathered Successfully</b></u> \n\n<b>Live Price of <code>{lmao}</code> is:-  $</b> <code>{a}</code> USD", parse_mode="HTML",)
+      else:
+        await event.edit(
+          f"<b><u>Stock Information Gathered Successfully</b></u> \n\n<b>Live Price of <code>{company}</code> is:-  $</b> <code>{a}</code> USD", parse_mode="HTML",)
+    except AssertionError:
+      await event.edit("There Is No Such Ticker.")
+    
