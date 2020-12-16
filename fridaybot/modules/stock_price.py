a355d79a205d613d7e0b6feb14f3cca5f0c6329d
Starkgangs <Starkgangz@gmail.com>
2020-12-16 04:21:30 +0000

    pylint: auto fixes

--- fridaybot/modules/stock_price.py
+++ fridaybot/modules/stock_price.py
@@ -1,10 +1,7 @@
+import requests
 from yahoo_fin import stock_info as si
 
-from uniborg.util import friday_on_cmd
-
-from fridaybot import CMD_HELP
 from fridaybot.utils import admin_cmd
-import requests
 
 
 @friday.on(admin_cmd(pattern="liveprice (.*)"))
@@ -13,24 +10,28 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     try:
-      a = si.get_live_price(input_str)
+        a = si.get_live_price(input_str)
 
+        def get_symbol(symbol):
+            url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(
+                symbol
+            )
+            result = requests.get(url).json()
+            for x in result["ResultSet"]["Result"]:
+                if x["symbol"] == symbol:
+                    return x["name"]
 
-      def get_symbol(symbol):
-        url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
-        result = requests.get(url).json()
-        for x in result['ResultSet']['Result']:
-          if x['symbol'] == symbol:
-            return x['name']
-
-      lmao = input_str.upper()
-      company = get_symbol(lmao)
-      if company==None:
-        await event.edit(
-          f"<b><u>Stock Information Gathered Successfully</b></u> \n\n<b>Live Price of <code>{lmao}</code> is:-  $</b> <code>{a}</code> USD", parse_mode="HTML",)
-      else:
-        await event.edit(
-          f"<b><u>Stock Information Gathered Successfully</b></u> \n\n<b>Live Price of <code>{company}</code> is:-  $</b> <code>{a}</code> USD", parse_mode="HTML",)
+        lmao = input_str.upper()
+        company = get_symbol(lmao)
+        if company == None:
+            await event.edit(
+                f"<b><u>Stock Information Gathered Successfully</b></u> \n\n<b>Live Price of <code>{lmao}</code> is:-  $</b> <code>{a}</code> USD",
+                parse_mode="HTML",
+            )
+        else:
+            await event.edit(
+                f"<b><u>Stock Information Gathered Successfully</b></u> \n\n<b>Live Price of <code>{company}</code> is:-  $</b> <code>{a}</code> USD",
+                parse_mode="HTML",
+            )
     except AssertionError:
-      await event.edit("There Is No Such Ticker.")
-    
+        await event.edit("There Is No Such Ticker.")
