f39d5e0baf4f2df2cd22351415378894773029d0
StarkGang <StarkGangZ@gmail.com>
2020-12-17 10:23:01 +0530

    Typo

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -210,7 +210,7 @@ async def take_screen_shot(
 # Thanks To @HeisenbergTheDanger, @xditya
 async def fetch_feds(event, borg):
     fedList = []
-    await event.edit("`Getting Your FeD List`")
+    await event.edit("`Fetching Your FeD List`, This May Take A While.")
     async with borg.conversation("@MissRose_bot") as bot_conv:
         await bot_conv.send_message("/start")
         await bot_conv.send_message("/myfeds")
@@ -221,6 +221,7 @@ async def fetch_feds(event, borg):
             await event.edit("`Try again after 5 mins.`")
             return
         elif "make a file" in response.text:
+            await event.edit('`Boss, You Real Peru. You Are Admin in So Many Feds. WoW!`')
             await asyncio.sleep(6)
             await response.click(0)
             await asyncio.sleep(6)
@@ -252,5 +253,5 @@ async def fetch_feds(event, borg):
 
                 elif In:
                     tempFedId += x
-    await event.edit("`FeD List Fetched SucessFully`")
+    await event.edit("`FeD List Fetched SucessFully.`")
     return fedList
