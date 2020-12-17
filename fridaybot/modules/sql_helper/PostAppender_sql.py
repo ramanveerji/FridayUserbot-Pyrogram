e6b4c4fab3cc430935a85efcc902aebaa2e51630
StarkGang <StarkGangZ@gmail.com>
2020-12-17 21:19:23 +0530

    Update PostAppender_sql.py

--- fridaybot/modules/sql_helper/PostAppender_sql.py
+++ fridaybot/modules/sql_helper/PostAppender_sql.py
@@ -42,8 +42,7 @@ def is_data_indbs(chat_id: int):
 
 
 def remove_datas(chat_id):
-    s__ = SESSION.query(Pa).get(str(chat_id))
-    saved_data = s__.chat_id, s__.append_foot, s__.textto_append
-    if s__:
-        SESSION.delete(saved_data)
+    lul = SESSION.query(Pa).get(str(chat_id))
+    if lul:
+        SESSION.delete(lul)
         SESSION.commit()
