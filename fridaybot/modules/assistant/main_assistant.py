68298faadd7bf5a429d4f0fb88210e7b00f379bf
StarkGang <StarkGangZ@gmail.com>
2020-12-17 11:36:53 +0530

    Update main_assistant.py

--- fridaybot/modules/assistant/main_assistant.py
+++ fridaybot/modules/assistant/main_assistant.py
@@ -18,6 +18,7 @@ import re
 
 from telethon import Button, custom, events
 from telethon.tl.functions.users import GetFullUserRequest
+from telethon.utils import pack_bot_file_id
 
 from fridaybot import bot
 from fridaybot.Configs import Config
@@ -135,7 +136,6 @@ async def all_messages_catcher(event):
         return
     else:
         await event.get_sender()
-        event.chat_id
         sed = await event.forward_to(bot.uid)
         # Add User To Database ,Later For Broadcast Purpose
         # (C) @SpecHide
@@ -150,24 +150,25 @@ async def sed(event):
     msg.id
     msg_s = event.raw_text
     user_id, reply_message_id = his_userid(msg.id)
-    if event.sender_id == bot.uid:
-        if event.raw_text.startswith("/"):
-            return
-        if event.text is not None and event.media:
-            bot_api_file_id = pack_bot_file_id(event.media)
-            await tgbot.send_file(
-                user_id,
-                file=bot_api_file_id,
-                caption=event.text,
-                reply_to=reply_message_id,
-            )
-        else:
-            msg_s = event.raw_text
-            await tgbot.send_message(
-                user_id,
-                msg_s,
-                reply_to=reply_message_id,
-            )
+    if event.sender_id != bot.uid:
+        return
+    elif event.raw_text.startswith("/"):
+        return
+    elif event.text is not None and event.media:
+        bot_api_file_id = pack_bot_file_id(event.media)
+        await tgbot.send_file(
+            user_id,
+            file=bot_api_file_id,
+            caption=event.text,
+            reply_to=reply_message_id,
+        )
+    else:
+        msg_s = event.raw_text
+        await tgbot.send_message(
+            user_id,
+            msg_s,
+            reply_to=reply_message_id,
+        )
 
 
 @assistant_cmd("broadcast", is_args=True)
@@ -223,8 +224,6 @@ async def starkislub(event):
 async def starkisnoob(event):
     if event.sender_id == bot.uid:
         msg = await event.get_reply_message()
-        msg.id
-        event.raw_text
         user_id, reply_message_id = his_userid(msg.id)
     if is_he_added(user_id):
         await event.reply("Already Blacklisted")
