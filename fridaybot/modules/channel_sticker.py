edbfceeef1bc4eafc31881f5b357930977a719c4
StarkGang <StarkGangZ@gmail.com>
2020-12-18 10:37:13 +0530

    Update channel_sticker.py

--- fridaybot/modules/channel_sticker.py
+++ fridaybot/modules/channel_sticker.py
@@ -68,7 +68,7 @@ async def lul(event):
     lsb = event.chat_id
     id_s = event.chat_id
     if is_data_indb(event.chat_id):
-        if any(x in lulstark for x in event.text.startswith):
+        if event.text.startswith('.'):
             return
         try:
             await borg.send_file(event.chat_id, is_data_indb(event.chat_id))
