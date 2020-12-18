7a813b377e4b0f143d899ec04a0c124038b3039a
StarkGang <StarkGangZ@gmail.com>
2020-12-18 13:53:02 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -11,7 +11,10 @@ from typing import List, Optional, Tuple
 
 from pymediainfo import MediaInfo
 from telethon.tl.types import MessageMediaPhoto
+import requests
+from bs4 import BeautifulSoup as bs
 
+BASE_URL = "https://isubtitles.org"
 from fridaybot.Configs import Config
 
 sedpath = Config.TMP_DOWNLOAD_DIRECTORY
@@ -257,3 +260,52 @@ async def fetch_feds(event, borg):
                     tempFedId += x
     await event.edit("`FeD List Fetched SucessFully.`")
     return fedList
+
+# Ported From https://github.com/Anandpskerala/SubtitlesBot
+def search_sub(query):
+    r = requests.get(f"{BASE_URL}/search?kwd={query}").text
+    soup = bs(r, "lxml")
+    list_search = soup.find_all("div", class_="row")
+    index = []
+    title = []
+    keywords = []
+
+    second_soup = bs(str(list_search), 'lxml')
+    headings = second_soup.find_all("h3")
+
+    third_soup = bs(str(headings), "lxml")
+    search_links = third_soup.find_all("a")
+
+    i = 0
+
+    for a in search_links:
+        i += 1
+        index.append(i)
+        title.append(a.text)
+        key = a.get("href").split("/")
+        keywords.append(key[1])
+
+    return index, title, keywords
+
+
+def get_lang(keyword):
+    url = f"{BASE_URL}/{keyword}"
+    request = requests.get(url).text
+    fourth_soup = bs(request, "lxml")
+    filesoup = fourth_soup.find_all("table")
+    fifth_soup = bs(str(filesoup), "lxml")
+    table_soup = fifth_soup.find_all("a")
+    language = []
+    index = []
+    link = []
+    i = 0
+    for b in table_soup:
+        if b["href"].startswith("/download/"):
+            i += 1
+            h = b.get("href").split("/")
+            buttoname = h[3]
+            if buttoname not in language:
+                index.append(i)
+                language.append(buttoname)
+                link.append(f"{BASE_URL}{b.get('href')}")
+    return index, language, link
