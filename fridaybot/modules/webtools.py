d7c9cc09fddea2e9fbf61a52ffe1f15670fe42ee
StarkGang <StarkGangZ@gmail.com>
2020-12-18 15:29:49 +0530

    Update webtools.py

--- fridaybot/modules/webtools.py
+++ fridaybot/modules/webtools.py
@@ -159,20 +159,6 @@ async def _(event):
         await event.edit("Some Thing Went Wrong.")
 
 
-@friday.on(friday_on_cmd(pattern="subtitle ?(.*)"))
-async def warnerbros(event):
-    msg = await event.edit("`Fetching..`")
-    warner_media = event.pattern_match.group(1)
-    full_index, title, keyword = search_sub(warner_media)
-    index, language, link = get_lang(keyword[0])
-    await borg.send_file(
-        event.chat_id,
-        link,
-        caption=f"**Video Title :** `{title[0]}` \n**Language :** `{language}` \n**Download Link :** `{link}`",
-    )
-    await msg.delete()
-
-
 CMD_HELP.update(
     {
         "webtools": "**Web Tools**\
