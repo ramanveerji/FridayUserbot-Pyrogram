50517b8734891d1dbfad19159b27709372739f15
Starkgangs <Starkgangz@gmail.com>
2020-12-19 01:59:25 +0000

    pylint: auto fixes

--- fridaybot/modules/movie_recomendation.py
+++ fridaybot/modules/movie_recomendation.py
@@ -1,11 +1,10 @@
-from bs4 import BeautifulSoup as SOUP 
-import re 
-import requests as HTTP 
-from uniborg.util import friday_on_cmd
+import re
+
+import requests as HTTP
+from bs4 import BeautifulSoup as SOUP
+
 from fridaybot import CMD_HELP
 from fridaybot.utils import admin_cmd
-
-
 
 
 @friday.on(admin_cmd(pattern="rmovie (.*)"))
@@ -13,67 +12,63 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    def main(emotion): 
-      if(emotion == "Sad"): 
-        urlhere = 'http://www.imdb.com/search/title?genres=drama&title_type=feature&sort=moviemeter, asc'
 
+    def main(emotion):
+        if emotion == "Sad":
+            urlhere = "http://www.imdb.com/search/title?genres=drama&title_type=feature&sort=moviemeter, asc"
 
-      elif(emotion == "Anticipation"): 
-        urlhere = 'https://www.imdb.com/search/title/?genres=sci-fi'
- 
-      elif(emotion == "Fear"): 
-        urlhere = 'http://www.imdb.com/search/title?genres=sport&title_type=feature&sort=moviemeter, asc'
+        elif emotion == "Anticipation":
+            urlhere = "https://www.imdb.com/search/title/?genres=sci-fi"
 
+        elif emotion == "Fear":
+            urlhere = "http://www.imdb.com/search/title?genres=sport&title_type=feature&sort=moviemeter, asc"
 
-      elif(emotion == "Enjoyment"): 
-        urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
+        elif emotion == "Enjoyment":
+            urlhere = "http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc"
 
-      elif(emotion == "Trust"): 
-        urlhere = 'http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc'
+        elif emotion == "Trust":
+            urlhere = "http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc"
 
-      elif(emotion == "Romantic"):
-        urlhere = 'https://www.imdb.com/search/title/?genres=romance'
-      
-      elif(emotion == "Comedy"):
-        urlhere ="https://www.imdb.com/search/title/?genres=comedy"
-       
-      
-      
-      response = HTTP.get(urlhere) 
+        elif emotion == "Romantic":
+            urlhere = "https://www.imdb.com/search/title/?genres=romance"
 
-      data = response.text 
-      soup = SOUP(data, "lxml") 
-      title = soup.find_all("a", attrs = {"href" : re.compile(r'\/title\/tt+\d*\/')})
-      return title 
+        elif emotion == "Comedy":
+            urlhere = "https://www.imdb.com/search/title/?genres=comedy"
 
+        response = HTTP.get(urlhere)
+
+        data = response.text
+        soup = SOUP(data, "lxml")
+        title = soup.find_all("a", attrs={"href": re.compile(r"\/title\/tt+\d*\/")})
+        return title
 
     emotion = input_str
-    a = main(emotion) 
+    a = main(emotion)
     count = 0
-    sed = ''
-    if(emotion == "Disgust" or emotion == "Anger" or emotion=="Surprise"): 
-      for i in a: 
-        tmp = str(i).split('>;') 
-        if(len(tmp) == 3): 
-          lol = tmp[1][:-3]
-          sed += lol + "\n"
-          if(count > 13): 
-            break
-          count += 1
+    sed = ""
+    if emotion == "Disgust" or emotion == "Anger" or emotion == "Surprise":
+        for i in a:
+            tmp = str(i).split(">;")
+            if len(tmp) == 3:
+                lol = tmp[1][:-3]
+                sed += lol + "\n"
+                if count > 13:
+                    break
+                count += 1
 
-    else: 
-      for i in a: 
-        tmp = str(i).split('>') 
-        if(len(tmp) == 3): 
-          lol = tmp[1][:-3]
-          sed += lol + "\n"
-        if(count > 11): 
-          break
-        count+=1
+    else:
+        for i in a:
+            tmp = str(i).split(">")
+            if len(tmp) == 3:
+                lol = tmp[1][:-3]
+                sed += lol + "\n"
+            if count > 11:
+                break
+            count += 1
     await event.edit(
-      f"<b><u>Below Are Your Movie Recommendations</b></u>\n\n<b>Your Emotion:- <code>{input_str}</code>\n<b>Recommended Movie List:- </b><code>{sed}</code>", parse_mode="HTML",)
-  
-
+        f"<b><u>Below Are Your Movie Recommendations</b></u>\n\n<b>Your Emotion:- <code>{input_str}</code>\n<b>Recommended Movie List:- </b><code>{sed}</code>",
+        parse_mode="HTML",
+    )
 
 
 CMD_HELP.update(
