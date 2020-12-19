4d946c822cc5921bab90d1079c0fb6e40eec3bcb
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2020-12-19 07:38:24 +0530

    Sangmata fix.

--- fridaybot/modules/sangmata.py
+++ fridaybot/modules/sangmata.py
@@ -27,53 +27,19 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=461843263)
             )
-            # await borg.forward_messages(chat, reply_message)
-            await silently_send_message(chat, "/generate")
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("```Please unblock @sangmatainfo_bot and try again```")
-            return
-        if response.text.startswith("Forward"):
-            await event.edit(
-                "```can you kindly disable your forward privacy settings for good?```"
-            )
-        else:
-            await event.edit(f"{response.message.message}")
-
-
-@friday.on(friday_on_cmd("fakemail ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    if not event.reply_to_msg_id:
-        await event.edit("```Reply to any user message.```")
-        return
-    reply_message = await event.get_reply_message()
-    if not reply_message.text:
-        await event.edit("```reply to text message```")
-        return
-    chat = "@fakemailbot"
-    reply_message.sender
-    if reply_message.sender.bot:
-        await event.edit("```Reply to actual users message.```")
-        return
-    await event.edit("```Processing```")
-    async with borg.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=177914997)
-            )
             await borg.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Please unblock @fakemailbot and try again```")
+            await event.reply("```Please unblock @SangMataInfo_bot and try again```")
             return
-        if response.text.startswith("send"):
+        if response.text.startswith("Hello,"):
             await event.edit(
                 "```can you kindly disable your forward privacy settings for good?```"
             )
         else:
             await event.edit(f"{response.message.message}")
+
+
 
 
 @friday.on(friday_on_cmd("ub ?(.*)"))
