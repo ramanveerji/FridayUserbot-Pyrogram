4a43e45e5876d0b6943a98cb4ef8bfc5d1a4a544
スタークギャング <StarkGangZ@gmail.com>
2021-01-15 09:56:15 +0530

    Update webtools.py

--- fridaybot/modules/webtools.py
+++ fridaybot/modules/webtools.py
@@ -26,7 +26,18 @@ async def _(event):
         file=imgpath,
         caption=f"**WEBSHOT OF** `{urlissed}` \n**Powered By @Fridayot**",
     )
-
+    
+    
+@friday.on(friday_on_cmd(pattern="rmeme$"))
+@friday.on(sudo_cmd(pattern="rmeme$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    hmm_s = 'https://some-random-api.ml/meme'
+    r = requests.get(url=hmm_s).json()
+    image_s = r['image']
+    await borg.send_file(event.chat_id, file=image_s, caption=r['caption'])
+    
 
 @friday.on(friday_on_cmd(pattern="lp ?(.*)"))
 @friday.on(sudo_cmd(pattern="lp ?(.*)", allow_sudo=True))
