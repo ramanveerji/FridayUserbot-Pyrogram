0b31c7dc3cacd9393514c1beac4cf0ba88d65287
スタークギャング <StarkGangZ@gmail.com>
2021-01-17 10:45:12 +0530

    Update translate.py

--- fridaybot/modules/translate.py
+++ fridaybot/modules/translate.py
@@ -6,7 +6,6 @@ Available Commands:
 from deep_translator import GoogleTranslator
 from googletrans import LANGUAGES
 from langdetect import detect
-from google_trans_new import google_translator
 import requests
 from fridaybot import CMD_HELP
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
