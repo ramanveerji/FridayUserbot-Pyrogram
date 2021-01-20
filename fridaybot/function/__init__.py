344a3ba7ae669c282f0d56f93454bfa8b7f39dae
スタークギャング <StarkGangZ@gmail.com>
2021-01-20 19:38:44 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -6,7 +6,7 @@ import hachoir
 import asyncio
 import os
 from pathlib import Path
-from fridaybot.utils import friday_on_cmd, load_module
+from fridaybot.utils import load_module
 from telethon.tl.types import DocumentAttributeAudio
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (
