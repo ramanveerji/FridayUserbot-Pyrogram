840f4459b3885186401c22c748e30f74120c8714
スタークギャング <StarkGangZ@gmail.com>
2021-01-20 17:27:27 +0530

    Update _helper.py

--- fridaybot/modules/_helper.py
+++ fridaybot/modules/_helper.py
@@ -1,7 +1,8 @@
 from fridaybot import CMD_LIST
+from fridaybot.utils import friday_on_cmd
 
 
-@command(pattern="^.help ?(.*)")
+@borg.on(friday_on_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
