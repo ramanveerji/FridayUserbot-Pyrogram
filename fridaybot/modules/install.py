0ecbb122c7aa26809d8d7d2a597e7f7da84947a5
スタークギャング <StarkGangZ@gmail.com>
2021-01-20 20:07:41 +0530

    Update install.py

--- fridaybot/modules/install.py
+++ fridaybot/modules/install.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 from pathlib import Path
 from fridaybot.utils import friday_on_cmd, load_module
-from .fridaybot.function import get_all_modules
+from fridaybot.function import get_all_modules
 from fridaybot import CMD_HELP
 
 DELETE_TIMEOUT = 5
