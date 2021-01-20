e1a2919a899c9cbb8e342c83b2d7852f444912d3
スタークギャング <StarkGangZ@gmail.com>
2021-01-20 19:30:40 +0530

    Update utils.py

--- fridaybot/utils.py
+++ fridaybot/utils.py
@@ -16,7 +16,6 @@ from fridaybot.wraptools import (
     ignore_pm,
 )
 from var import Var
-from fridaybot.function import strings as _tr
 sedprint = logging.getLogger("PLUGINS")
 cmdhandler = Config.COMMAND_HAND_LER
 bothandler = Config.BOT_HANDLER
@@ -123,7 +122,6 @@ def load_module(shortname):
         mod.tgbot = bot.tgbot
         mod.Var = Var
         mod.command = command
-        mod._tr = _tr
         mod.logger = logging.getLogger(shortname)
         # support for uniborg
         sys.modules["uniborg.util"] = fridaybot.utils
