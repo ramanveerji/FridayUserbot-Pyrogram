782b963d8a03536268019824fef7a606061279a9
StarkGang <starkgangz@gmail.com>
2021-01-20 04:52:57 +0000

    Update License

--- fridaybot/modules/sql_helper/broadcast_sql.py
+++ fridaybot/modules/sql_helper/broadcast_sql.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2020-2021 @HeisenbergTheDanger
+#    Copyright (C) 2020-2021-2021 @HeisenbergTheDanger
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
