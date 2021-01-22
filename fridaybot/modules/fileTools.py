7f4db8e8f440ba7ba7d98dd24c521a1b975feaee
スタークギャング <StarkGangZ@gmail.com>
2021-01-22 10:37:19 +0530

    Test

--- fridaybot/modules/fileTools.py
+++ fridaybot/modules/fileTools.py
@@ -4,6 +4,7 @@ import time
 import time as t
 import zipfile
 from datetime import datetime
+import shutil
 from fridaybot import CMD_HELP
 from fridaybot.function import convert_to_image, crop_vid, runcmd
 from fridaybot.utils import friday_on_cmd, sudo_cmd
@@ -17,7 +18,6 @@ import uuid
 
 if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
   os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-  
 
 @friday.on(friday_on_cmd(pattern="p2dcl(?: |$)(.*)"))
 async def starky(event):
@@ -34,55 +34,33 @@ async def starky(event):
         chnnl = un
     else:
         chnnl = event.chat_id
-    
     await event.edit(f"**Fetching All Files From This Channel**")
     try:
-        chnnl_msgs = await borg.get_messages(chnnl, limit=3000)
+        chnnl_msgs = await borg.get_messages(chnnl, limit=3000, search=".pdf")
     except:
         await event.edit("**Unable To fetch Messages !** \n`Please, Check Channel Details And IF THere Are Any Media :/`")
         return
-    
-    
     total = int(chnnl_msgs.total)
-    
-    await event.edit(f"**Downloading {total} Media/Messages**")
+    await event.edit(f"**Downloading {total} Pdf's**")
     for d in chnnl_msgs:
         if d.media:
             media_count += 1
             await borg.download_media(d.media, diro)
-        if d.text:
-            text_count += 1
-            
+        else:
+            pass
     await event.edit(f"**Total Media :** `{total}` \n**Downloaded Media :** `{media_count}` \n**Total Texts :** `{text_count}` \n**Now Converting Files.**")
-    
-    
     Azx = glob.glob(f"{diro}*.pdf")
-    
     for friday in Azx:
       N = 9
-      
       res =''.join(random.choices(string.ascii_uppercase+string.digits, k = N))
       pdf_file = friday
       docx_file = f'{dirb}{str(res)}.docx'
-      
       parse(pdf_file, docx_file, start=0, end=None)
-      
-      
-    Ax = glob.glob(f"{dirb}*.docx")
-    for pop in Ax:
-      await borg.send_file(event.chat_id, pop, caption=f"**Total Media :** `{total}` \n**Downloaded Media :** `{media_count}` \n**Total Texts  :** `{text_count}` \n**By @fridayot**")
-    Azx = glob.glob(f"{diro}*")
-    Azpx = glob.glob(f"{dirb}*")
-    for x in Azx:
-      os.remove(x)
-    
-    for pop in Azpx:
-      os.remove(pop)
-    
-    
-    os.rmdir(diro)
-    os.rmdir(dirb)
-
+    shutil.make_archive(f"FridayConverter", "zip", dirb)
+    await borg.send_file(event.chat_id, "FridayConverter.zip", caption=f"**Total PDF :** `{total}` \n**Downloaded PDF :** `{media_count}` \n**By @fridayot**")
+    os.remove("FridayConverter.zip")
+    shutil.rmtree(dirb)
+    shutil.rmtree(diro)
 
   
 @friday.on(friday_on_cmd(pattern=r"pdf2docx"))
@@ -94,14 +72,10 @@ async def hmm(event):
     hmmu = await event.reply("hmm... Please Wait...🚶")
     lol = await event.get_reply_message()
     starky = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
-    
-    hmmu = await event.reply("hmm... Please Wait...🚶")
-    
+    hmmu = await event.reply("hmm... Please Wait..")
     pdf_file = starky
     docx_file = './fridaybot/DOWNLOADS/FRIDAYOT.docx'
-    
     parse(pdf_file, docx_file, start=0, end=None)
-    
     await borg.send_file(
         event.chat_id, docx_file, caption=f"*PDF Converted Into Docx by Friday bot. Get your Friday From @FRIDAYOT."
     )
