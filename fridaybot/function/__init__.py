2db8edd859029c6ecafa72c4afafbab95b7a74a7
スタークギャング <StarkGangZ@gmail.com>
2021-01-22 22:03:23 +0530

    Update __init__.py

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -533,12 +533,12 @@ async def get_all_admin_chats(event):
             for d in await event.client.get_dialogs()
             if (d.is_group or d.is_channel)
         ]
-        for i in all_chats:
-            try:
-                if i.creator or i.admin_rights:
-                    lul_stark.append(i.id)
-            except:
-                pass
+    for i in all_chats:
+        try:
+            if i.creator or i.admin_rights:
+                lul_stark.append(i.id)
+        except:
+            pass
     return lul_stark
                   
 async def is_admin(event):
