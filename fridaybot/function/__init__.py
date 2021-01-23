400d618c55ad118e82e87797481f108da8f0952e
スタークギャング <StarkGangZ@gmail.com>
2021-01-23 09:48:57 +0530

    Typo

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -533,7 +533,7 @@ async def get_all_admin_chats(event):
             for d in await event.client.get_dialogs()
             if (d.is_group or d.is_channel)
         ]
-    for i in all_chats
+    for i in all_chats:
         if i.creator or i.admin_rights:
             lul_stark.append(i.id)
     return lul_stark
