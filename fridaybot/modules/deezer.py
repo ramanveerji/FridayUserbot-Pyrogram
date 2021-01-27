8aff229905bba56118e9dcc2860f9cfb17258e15
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-01-27 15:37:01 +0530

    New deezer.py plugin. Don't Kang it.🌚

---
+++ fridaybot/modules/deezer.py
@@ -0,0 +1,131 @@
+from fridaybot import CMD_HELP
+from fridaybot.utils import friday_on_cmd
+from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
+import asyncio
+import math
+import os
+import time
+from telethon.tl.types import DocumentAttributeAudio
+import os
+import requests
+fridaythumb = "./resources/IMG_20200929_103719_628.jpg"
+
+
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    """Generic progress_callback for uploads and downloads."""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            await event.edit(
+                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("{}\n{}".format(type_of_ps, tmp))
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+
+@friday.on(friday_on_cmd(pattern="deezer ?(.*)"))
+@friday.on(sudo_cmd(pattern="deezer ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    link = f"https://api.deezer.com/search?q={input_str}&limit=1"
+    ommhg = await edit_or_reply(event, "Searching For The Song")
+    dato = requests.get(url=link).json()
+    match = dato.get("data")
+    urlhp= (match[0])
+    urlp = urlhp.get("link")
+    polu = urlhp.get("artist")
+    replo = urlp[29:]
+    urlp = f"https://starkapi.herokuapp.com/deezer/{replo}"
+    
+    datto = requests.get(url=urlp).json()
+    mus = datto.get("url")
+    sname = f'''{urlhp.get("title")}.mp3'''
+    doc = requests.get(mus)
+    await ommhg.edit("Please Wait, I Am Downloading Thr Song. 😁😄")
+    with open(sname, 'wb') as f:
+      f.write(doc.content)
+    car = f"""
+**Song Name :** {urlhp.get("title")}
+**Duration :** {urlhp.get('duration')} Seconds
+**Artist :** {polu.get("name")}
+
+Music Downloaded And Uploaded By Friday Userbot
+
+Get Your Friday From @FridayOT"""
+    await ommhg.edit("Song Downloaded.  Waiting To Upload. 🥳🤗")
+    c_time = time.time()
+    
+    
+    await event.client.send_file(
+            event.chat_id,
+            sname,
+            supports_streaming=True,
+            caption=car,
+            attributes=[
+                DocumentAttributeAudio(
+                    duration=int(urlhp.get('duration')),
+                    title=str(urlhp.get("title")),
+                    performer=str(polu.get("name")),
+                )
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(
+                    d, t, event, c_time, "Uploading..", sname
+                )
+            ),
+        )
+    
+    
+    
+    os.remove(sname)
+    await event.delete()
