5c9e277bfb0e51c78868773924bbfd1fed13200f
スタークギャング <StarkGangZ@gmail.com>
2021-01-28 09:40:55 +0530

    Update waifu.py

--- fridaybot/modules/waifu.py
+++ fridaybot/modules/waifu.py
@@ -32,6 +32,8 @@ def deEmojify(inputString: str) -> str:
 @friday.on(friday_on_cmd(pattern=r"waifu(?: |$)(.*)"))
 @friday.on(sudo_cmd(pattern=r"waifu(?: |$)(.*)", allow_sudo=True))
 async def waifu(animu):
+    if event.fwd_from:
+        return
     text = animu.pattern_match.group(1)
     if not text:
         if animu.is_reply:
