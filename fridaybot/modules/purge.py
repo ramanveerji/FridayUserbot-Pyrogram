3ee82206ecfff816f3e735fa1b286b80535d9fc0
スタークギャング <StarkGangZ@gmail.com>
2021-01-28 10:36:56 +0530

    Update purge.py

--- fridaybot/modules/purge.py
+++ fridaybot/modules/purge.py
@@ -15,6 +15,8 @@ from fridaybot.utils import errors_handler, register, friday_on_cmd
 
 @friday.on(friday_on_cmd(pattern="purge$"))
 async def fastpurger(purg):
+    if purg.fwd_from:
+        return
     """ For .purge command, purge all messages starting from the reply. """
     chat = await purg.get_input_chat()
     msgs = []
@@ -45,6 +47,8 @@ async def fastpurger(purg):
 
 @friday.on(friday_on_cmd(pattern="purgeme"))
 async def purgeme(delme):
+    if delme.fwd_from:
+        return
     """ For .purgeme, delete x count of your latest message."""
     message = delme.text
     count = int(message[9:])
@@ -71,6 +75,8 @@ async def purgeme(delme):
 
 @friday.on(friday_on_cmd(pattern="del"))
 async def delete_it(delme):
+    if delme.fwd_from:
+        return
     """ For .del command, delete the replied message. """
     msg_src = await delme.get_reply_message()
     if delme.reply_to_msg_id:
@@ -90,6 +96,8 @@ async def delete_it(delme):
 
 @friday.on(friday_on_cmd(pattern="edit"))
 async def editer(edit):
+    if edit.fwd_from:
+        return
     message = edit.text
     chat = await edit.get_input_chat()
     self_id = await edit.client.get_peer_id("me")
@@ -109,6 +117,8 @@ async def editer(edit):
 
 @friday.on(friday_on_cmd(pattern="sd"))
 async def selfdestruct(destroy):
+    if destroy.fwd_from:
+        return
     """ For .sd command, make seflf-destructable messages. """
     message = destroy.text
     counter = int(message[4:6])
