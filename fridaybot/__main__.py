a6caaf70109b63460eab2613d3695967ef2a8500
スタークギャング <StarkGangZ@gmail.com>
2021-01-29 10:03:14 +0530

    Update __main__.py

--- fridaybot/__main__.py
+++ fridaybot/__main__.py
@@ -7,6 +7,7 @@ from telethon import TelegramClient
 
 from fridaybot import bot, client2, client3
 from fridaybot.Configs import Config
+from telethon.tl.types import InputMessagesFilterDocument
 from fridaybot.utils import load_module, start_assistant, load_module_dclient
 from var import Var
 
