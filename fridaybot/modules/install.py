55460aa842400fda210eca568a61a8e473b2bdfd
スタークギャング <StarkGangZ@gmail.com>
2021-01-30 18:45:21 +0530

    Update install.py

--- fridaybot/modules/install.py
+++ fridaybot/modules/install.py
@@ -40,6 +40,19 @@ async def install(event):
             os.remove(downloaded_file_name)
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
+    
+#    Copyright (C) FridayDevs 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 @borg.on(friday_on_cmd(pattern='pl ?(.*)'))
 async def _(event):
