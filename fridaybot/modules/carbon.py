42cf7b0ab469626cf00c194bc3441bfad98c32ff
スタークギャング <StarkGangZ@gmail.com>
2021-01-30 16:30:28 +0530

    Typo

--- fridaybot/modules/carbon.py
+++ fridaybot/modules/carbon.py
@@ -78,9 +78,7 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="<< `Here's your carbon!` \n **Carbonised Using** [Friday](https://github.com/Starkgang/FridayUserbot)>>".format(
-                color_name
-            ),
+            caption="<< `Here's your carbon!` \n **Carbonised Using** [Friday](https://github.com/Starkgang/FridayUserbot)>>",
             force_document=True,
             reply_to=e.message.reply_to_msg_id,
         )
