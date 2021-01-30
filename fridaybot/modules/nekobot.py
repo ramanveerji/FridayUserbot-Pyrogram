84a3cc769f1e49c3c64d058efb3ea9cb120e7284
スタークギャング <StarkGangZ@gmail.com>
2021-01-30 18:45:54 +0530

    Update nekobot.py

--- fridaybot/modules/nekobot.py
+++ fridaybot/modules/nekobot.py
@@ -1,7 +1,16 @@
-# Copyright (c) By Midhun KM [@StarkXD]
-# I Am Noob
-# Official Web : nekobot.xyz
-# "Copy It As You Want But Don't Edit Credits"
+#    Copyright (C) @chsaiujwal 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import requests
 from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
 
