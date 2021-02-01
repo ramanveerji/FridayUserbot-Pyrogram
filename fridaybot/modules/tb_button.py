83a21db3bd525e6f19b6313d3a0f927c82df1d41
スタークギャング <StarkGangZ@gmail.com>
2021-02-01 10:22:10 +0530

    Update tb_button.py

--- fridaybot/modules/tb_button.py
+++ fridaybot/modules/tb_button.py
@@ -14,7 +14,7 @@ BTN_URL_REGEX = re.compile(r"(\{([^\[]+?)\}\<buttonurl:(?:/{0,2})(.+?)(:same)?\>
 async def _(event):
     if event.fwd_from:
         return
-    if Config.TG_BOT_USER_NAME_BF_HER is None or tgbot is None:
+    if Config.TG_BOT_TOKEN_BF_HER is None or tgbot is None:
         await event.edit("need to set up a @BotFather bot for this module to work")
         return
 
