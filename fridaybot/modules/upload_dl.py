6153228c57dcc82232bead90954455bcb5d6c522
スタークギャング <StarkGangZ@gmail.com>
2021-02-01 15:47:52 +0530

    Update upload_dl.py

--- fridaybot/modules/upload_dl.py
+++ fridaybot/modules/upload_dl.py
@@ -447,6 +447,19 @@ async def lul(event):
     else:
         await mone.edit("Incorrect URL\n {}".format(input_str))
     
+#    Copyright (C) Midhun Km 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
 @friday.on(friday_on_cmd(pattern="zeelink"))
 async def lol_kangers(event):
     if event.fwd_from:
