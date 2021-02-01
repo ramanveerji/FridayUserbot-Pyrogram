45273e428eddf296caa404ca0c8c1165ddec41ec
スタークギャング <StarkGangZ@gmail.com>
2021-02-01 15:45:07 +0530

    Fix

--- fridaybot/modules/text_analysis.py
+++ fridaybot/modules/text_analysis.py
@@ -16,10 +16,10 @@ async def mar(event):
       'text': sadly.raw_text
       }
     r = requests.get("https://devsexpo.me/spam", headers=lol_s).json()
-    sed_rip = f"""<b><u>Text Scanned</b></u>
-<b>Text :</b> {sadly.text}
-<b>Spam Percentage :</b> {r['spam']}
-<b>Ham Percentage :</b> {r['ham']}
-<b>Prediction :</b> {r['prediction']}
+    sed_rip = f"""<b><u>Text Scanned SucessFully !</b></u>
+<b>Text :</b> <code>{sadly.raw_text}</code>
+<b>Spam Percentage :</b> <code>{r['spam']}</code>
+<b>Ham Percentage :</b> <code>{r['ham']}</code>
+<b>Prediction :</b> <code>{r['prediction']}</code>
 """
     await event.edit(sed_rip, parse_mode="HTML")
