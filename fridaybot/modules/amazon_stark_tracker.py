37eab62d0e12f18b577204fb42b58282e24dccfd
スタークギャング <StarkGangZ@gmail.com>
2021-02-03 14:48:22 +0530

    Update amazon_stark_tracker.py

--- fridaybot/modules/amazon_stark_tracker.py
+++ fridaybot/modules/amazon_stark_tracker.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from amazon_tracker_sql import add_new_tracker, get_all_tracker, is_tracker_in_db, rm_tracker, get_tracker_info, rm_tracker
+from sql_helper.amazon_tracker_sql import add_new_tracker, get_all_tracker, is_tracker_in_db, rm_tracker, get_tracker_info, rm_tracker
 import requests
 from bs4 import BeautifulSoup
 
