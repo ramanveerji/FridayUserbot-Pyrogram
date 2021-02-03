28313bdbbb5bd81a789e39ee4195110bee19e1d4
スタークギャング <StarkGangZ@gmail.com>
2021-02-03 13:07:19 +0530

    tests

---
+++ fridaybot/modules/amazon_price_tracker.py
@@ -0,0 +1,37 @@
+import asyncio
+from bs4 import BeautifulSoup
+import math
+import os
+import heroku3
+import requests
+url = Config.AMAZON_TRACK_URL
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+from fridaybot import bot
+price_needed = Config.BUDGET_AMAZON
+logger = Config.PRIVATE_GROUP_BOT_API_ID
+Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
+
+if url:
+  async def foo():
+      page = requests.get(url, headers = headers)
+      soup = BeautifulSoup(page.content, 'html.parser')
+      title = soup.find(id = "productTitle").get_text()
+      price = soup.find(id = "priceblock_ourprice").get_text()
+      title = title.strip()
+      price = price[2:].split(',')
+      price = float("".join(price))
+      if(price <= price_needed):
+          await bot.send_message(logger, "The current price of " + title + " is Rs " + str(price)) 
+          try:
+            app = Heroku.app(Config.HEROKU_APP_NAME)
+            heroku_var = app.config()
+            del heroku_var['AMAZON_TRACK_URL']
+            def heroku_var['BUDGET_AMAZON']
+          except:
+            pass
+      else:
+          return
+          
+  def foo_cb():
+      bot.loop.create_task(foo())
+  bot.loop.call_later(10 * 60, foo_cb)
