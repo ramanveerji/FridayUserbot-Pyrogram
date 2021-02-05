da53457f538286c8fee6b038fc6ddcbd0e0903ba
スタークギャング <StarkGangZ@gmail.com>
2021-02-05 14:18:48 +0530

    Rewrite : img plugin

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -19,6 +19,12 @@ import hachoir
 import asyncio
 import os
 from pathlib import Path
+from selenium import webdriver
+import time
+import requests
+import shutil
+import os
+import argparse
 import wget
 from fridaybot import bot as borg
 import lottie
@@ -709,3 +715,32 @@ async def fetch_audio(event, ws):
         )
     await event.edit("`Almost Done!`")    
     return final_warner
+
+def save_img(inp , img , i, directory):
+    try:
+        filename = inp + str(i) + '.jpg'
+        response = requests.get(img,stream=True)
+        image_path = os.path.join(directory, filename)
+        with open(image_path, 'wb') as file:
+            shutil.copyfileobj(response.raw, file)
+    except Exception:
+        pass
+
+
+def find_urls(inp,url,driver, directory):
+    driver.get(url)
+    for _ in range(500):
+        driver.execute_script("window.scrollBy(0,10000)")
+        try:
+            driver.find_element_by_css_selector('.mye4qd').click()
+        except:
+            continue
+    for j, imgurl in enumerate(driver.find_elements_by_xpath('//img[contains(@class,"rg_i Q4LuWd")]')):
+        try:
+            imgurl.click()
+            img = driver.find_element_by_xpath('//body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+            save_img(inp,img,j, directory)
+            time.sleep(1.5)
+        except:
+            pass
+            
