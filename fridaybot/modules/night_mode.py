86e4d139bc93f5ad2027fe11ab82c847ed47ae82
スタークギャング <StarkGangZ@gmail.com>
2021-02-05 10:02:17 +0530

    Fixed
    Ye bhi kang karle white eye kanger

--- fridaybot/modules/night_mode.py
+++ fridaybot/modules/night_mode.py
@@ -98,16 +98,16 @@ async def job_close():
             functions.messages.EditChatDefaultBannedRightsRequest(
                 peer=warner, banned_rights=hehes
             )
+            )
             if Config.CLEAN_GROUPS:
                 async for user in friday.iter_participants(warner):
                     if user.deleted:
                         await friday.edit_permissions(warner, user.id, view_messages=False)
-        )
         except:
             pass
 
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_close, trigger="cron", hour=9, minute=59)
+scheduler.add_job(job_close, trigger="cron", hour=10, minute=15)
 scheduler.start()
 
 
@@ -134,5 +134,5 @@ async def job_open():
 
 # Run everyday at 06
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_open, trigger="cron", hour=10, minute=10)
+scheduler.add_job(job_open, trigger="cron", hour=10, minute=20)
 scheduler.start()
