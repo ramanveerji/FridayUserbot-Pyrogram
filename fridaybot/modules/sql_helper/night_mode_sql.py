1b71678d3a64d5ad3c6dd03e5d1979e6c8a716c9
スタークギャング <StarkGangZ@gmail.com>
2021-02-05 12:13:34 +0530

    lol

--- fridaybot/modules/sql_helper/night_mode_sql.py
+++ fridaybot/modules/sql_helper/night_mode_sql.py
@@ -11,7 +11,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from sqlalchemy import Column, UnicodeText, Integer
+from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from fridaybot.modules.sql_helper import BASE, SESSION
 
 
