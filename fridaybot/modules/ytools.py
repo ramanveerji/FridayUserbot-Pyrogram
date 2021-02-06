ad80d8eee3e9e5ec98700604c701abb610ef5a9e
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-02-06 19:56:02 +0530

    Update ytools.py

--- fridaybot/modules/ytools.py
+++ fridaybot/modules/ytools.py
@@ -199,12 +199,97 @@ async def _(event):
         if files and os.path.exists(files):
             os.remove(files)
 
+
+@friday.on(friday_on_cmd(pattern="ytm4a ?(.*)"))
+@friday.on(sudo_cmd(pattern="ytm4a ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    urlissed = event.pattern_match.group(1)
+    myself_stark = await edit_or_reply(
+        event, f"`Getting {urlissed} From Youtube Servers. Please Wait.`"
+    )
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    if not os.path.isdir("./music/"):
+        os.makedirs("./music/")
+    path = Config.TMP_DOWNLOAD_DIRECTORY
+    url = mo
+    sedlyf = wget.download(kekme, out=path)
+    opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "m4a",
+                    "preferredquality": "720",
+                }
+            ],
+            "outtmpl": "%(title)s.m4a",
+            "quiet": True,
+            "logtostderr": False,
+        }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url)
+    except Exception as e:
+        await event.edit(f"**Failed To Download** \n**Error :** `{str(e)}`")
+        return
+    await asyncio.sleep(20)
+    c_time = time.time()
+    file_stark = f"{ytdl_data['title']}.m4a"
+    lol_m = await upload_file(
+            file_name=file_stark,
+            client=borg,
+            file=open(file_stark, 'rb'),
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(
+                    d, t, event, c_time, "Uploading Your Song!", file_stark
+                )
+            ),
+        )
+    capy = f"**Song Name ➠** `{thum}` \n**Requested For ➠** `{urlissed}` \n**Channel ➠** `{thums}` \n**Link ➠** `{mo}`"
+    await event.delete()
+    await borg.send_file(
+        event.chat_id,
+        lol_m,
+        force_document=False,
+        allow_cache=False,
+        caption=capy,
+        thumb=sedlyf,
+        attributes=[
+                DocumentAttributeAudio(
+                    duration=int(ytdl_data["duration"]),
+                    title=str(ytdl_data["title"]),
+                    performer=str(ytdl_data["uploader"]),
+                )
+            ],
+        supports_streaming=True,
+    )
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
             
 
 CMD_HELP.update(
     {
         "ytmusic": "**Ytmusic**\
 \n\n**Syntax : **`.ytmusic <song name>`\
-\n**Usage :** Downloads songs from ytmusic"
+\n**Usage :** Downloads songs from ytmusic\
+\n\n**Syntax : **`.ytm4a <song name>`\
+\n**Usage :** Downloads songs from ytmusic with best quality"
     }
 )
