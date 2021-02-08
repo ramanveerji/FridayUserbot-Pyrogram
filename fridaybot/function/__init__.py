e38c23dd49c4bd33e7ac88a3d6a922b73f7425ab
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-02-08 20:56:42 +0530

    Fix

--- fridaybot/function/__init__.py
+++ fridaybot/function/__init__.py
@@ -10,7 +10,7 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+import random
 import requests
 from bs4 import BeautifulSoup
 from hachoir.metadata import extractMetadata
