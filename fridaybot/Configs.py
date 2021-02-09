24193d3b48b7a20ba1bc4837036b656e45ee25c7
スタークギャング <StarkGangZ@gmail.com>
2021-02-09 19:25:04 +0530

    Update Configs.py

--- fridaybot/Configs.py
+++ fridaybot/Configs.py
@@ -9,6 +9,7 @@ elif ENV:
     class Config(object):
         LOGGER = True
         APP_ID = int(os.environ.get("APP_ID", 6))
+        TZ = os.environ.get("TZ", "Asia/Kolkata")
         API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
         CLEAN_GROUPS = os.environ.get("CLEAN_GROUPS", False)
         STRING_SESSION = os.environ.get("STRING_SESSION", None)
