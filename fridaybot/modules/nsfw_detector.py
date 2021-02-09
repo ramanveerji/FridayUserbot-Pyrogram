26615fe954e0be84723cf5c13fa9f601a5ca5275
スタークギャング <StarkGangZ@gmail.com>
2021-02-09 12:31:36 +0530

    Update nsfw_detector.py

--- fridaybot/modules/nsfw_detector.py
+++ fridaybot/modules/nsfw_detector.py
@@ -1,3 +1,16 @@
+#    Copyright (C) @chsaiujwal 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from fridaybot.utils import friday_on_cmd
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 from fridaybot.function import convert_to_image, is_nsfw
