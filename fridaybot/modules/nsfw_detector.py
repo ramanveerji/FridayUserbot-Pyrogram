1644bc9a096d20a5fdb8ddc24f5c4d46ef8400f9
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-02-09 07:03:49 +0530

    Hopefully correct.

--- fridaybot/modules/nsfw_detector.py
+++ fridaybot/modules/nsfw_detector.py
@@ -1,6 +1,6 @@
 from fridaybot.utils import friday_on_cmd
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
-from fridaybot.function import convert_to_image
+from fridaybot.function import convert_to_image, is_nsfw
 import requests
 import string 
 import random 
@@ -15,36 +15,15 @@ async def _(event):
         await edit_or_reply(event, "Reply To Any Image Idiot.")
         return
     reply_message = await event.get_reply_message()
-    Credits = "By Friday. Get Your Friday From @Friday_OT"
-    Reply_message = Credits
-    if not reply_message.media:
-      await edit_or_reply(event, "Reply To Any Image. Not To text or anything else.")
+    kok = await edit_or_reply(event, "`Processing...`")
+    IdkWtf = is_nsfw(reply_message)
+    if IdkWtf is False:
+      await kok.edit("Replied Image Is Not Nsfw)
       return
-    tokez = Reply_message[3:9].lower()
-    loZ = Reply_message[3].lower()
-    nsfew = "nsfw[001][5556]^√~~×{{}∆}÷]][™™®®®--44447££6"
-    kok = await edit_or_reply(event, "Processing.")
-    nsf=  nsfew[2]
-    if loZ == nsf:
-      N = 15
-    else:
-      N = 14
-    img = await convert_to_image(event, borg)
-    res = ''.join(random.choices(string.ascii_uppercase +string.digits, k = N))
-    token = str(res)
-    f = {"file": (img, open(img, "rb"))}
-    h = {
-      "by":tokez,
-      "token":token
-    }
-    r = requests.post("https://starkapi.herokuapp.com/nsfw/", files = f, headers = h).json()
-    if r.get("success") is False:
-      await kok.edit(r.get("error"))
-      return
-    elif r.get("is_nsfw") is True:
+    elif IdkWtf is True:
       await kok.edit("Replied Image is nsfw.")
-    elif r.get("is_nsfw") is False:
-      await kok.edit("Replied Image is Not nsfw.")
+      return 
+    
 
 
 CMD_HELP.update(
