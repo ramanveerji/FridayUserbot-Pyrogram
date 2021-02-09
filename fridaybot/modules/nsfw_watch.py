cd3f4b8099e261a43e8e0f3f5c57465dc73a25cc
スタークギャング <StarkGangZ@gmail.com>
2021-02-09 12:59:43 +0530

    Import Unimports

--- fridaybot/modules/nsfw_watch.py
+++ fridaybot/modules/nsfw_watch.py
@@ -11,7 +11,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from telethon import functions
+rom telethon import Button, custom, events, functions
 from fridaybot.function import get_all_admin_chats, is_admin, is_nsfw
 import requests
 import string 
