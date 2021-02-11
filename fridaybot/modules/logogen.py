3466d673299333371b6cd08726e12069941f580f
スタークギャング <StarkGangZ@gmail.com>
2021-02-11 11:18:14 +0530

    Update logogen.py

--- fridaybot/modules/logogen.py
+++ fridaybot/modules/logogen.py
@@ -1,3 +1,16 @@
+#    Copyright (C) @chsaiujwal 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from fridaybot.utils import friday_on_cmd
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 from fridaybot.function import convert_to_image
@@ -12,6 +25,7 @@ async def _(event):
     if event.fwd_from:
         return
     input_st = event.pattern_match.group(1)
+    await event.edit("`Processing...`")
     Credits = "By FridayBot. Get Your FridayBot From @FridayOT."
     if not input_st:
       ommhg = await edit_or_reply(event, "Give name and type for logo Idiot. like `.logogen messi:football`")
