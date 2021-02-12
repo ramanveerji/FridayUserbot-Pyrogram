ada706391eddca9df6c766181723b9d27acfed27
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-02-12 17:05:23 +0530

    Hitler is great. Hail Hitler. ;/

--- fridaybot/modules/maigret.py
+++ fridaybot/modules/maigret.py
@@ -1,3 +1,17 @@
+#    Copyright (C) @DevsExpo 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from fridaybot.utils import friday_on_cmd
 from fridaybot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 from fridaybot.function import convert_to_image
@@ -10,10 +24,13 @@ import sys
 
 #Hitler is Great!
 #Hail Hitler
+#Hitler is Great!
+#Hail Hitler
 Hitler = "/reports/"
 if os.path.isdir(Hitler):
     os.rmdir(Hitler)
-
+#Hitler is Great!
+#Hail Hitler
 @friday.on(friday_on_cmd(pattern="maigret ?(.*)"))
 @friday.on(sudo_cmd(pattern="maigret ?(.*)", allow_sudo=True))
 async def _(event):
@@ -49,9 +66,12 @@ async def _(event):
         silent=True,
     )
     await ommhg.delete()
-
-
-
+#Hitler is Great!
+#Hail Hitler
+#Hitler is Great!
+#Hail Hitler
+#Hitler is Great!
+#Hail Hitler
 
 CMD_HELP.update(
     {
