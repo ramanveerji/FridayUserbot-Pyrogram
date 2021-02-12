a924755476a2510ae35be29406e252e63df1d2c5
スタークギャング <StarkGangZ@gmail.com>
2021-02-12 14:15:01 +0530

    Update image_vidtools.py

--- fridaybot/modules/image_vidtools.py
+++ fridaybot/modules/image_vidtools.py
@@ -626,10 +626,10 @@ async def yufytf(event):
     text = event.pattern_match.group(2)
     img = Image.open('./resources/Blankmeisnub.jpg')
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('Fonts/Streamster.ttf', 300)
+    font = ImageFont.truetype('Fonts/Streamster.ttf', 250)
     image_widthz, image_heightz = img.size
     w,h = font.getsize(text)
-    draw.text((image_widthz-w/2, image_heightz-h/2), text, font=font, fill=(255, 255, 0))
+    draw.text(((image_widthz-w/2), (image_heightz-h/2)), text, font=font, fill=(255, 255, 0))
     file_name = "LogoBy@MeisNub.png"
     await event.delete()
     ok = sedpath + "/" + file_name
