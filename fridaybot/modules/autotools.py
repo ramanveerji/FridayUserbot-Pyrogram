66ca7a4223110dd31b992ec33a4a995f8c8dbb88
スタークギャング <StarkGangZ@gmail.com>
2021-02-12 20:06:38 +0530

    Final Fix

--- fridaybot/modules/autotools.py
+++ fridaybot/modules/autotools.py
@@ -1,6 +1,18 @@
+#    Copyright (C) @DevsExpo 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 import time
-
 from telethon.errors import FloodWaitError
 from telethon.tl import functions
 from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
@@ -16,7 +28,7 @@ scheduler = AsyncIOScheduler(executors={'default': AsyncIOExecutor()})
 async def _(event):
     if event.fwd_from:
         return
-    sed = await edit_or_reply(event, "`Started AutoName Your Name Will Be Changed Every 1 Min, According To TimeZone Given. To Terminate This Process Do A Restart`")
+    sed = await edit_or_reply(event, "`Started AutoName Your Name Will Be Changed Every 1 Min, According To TimeZone Given. To Terminate This Process Use .stop Cmd`")
     scheduler.add_job(auto_name, 'interval', args=[event.pattern_match.group(1)], minutes=1, id='autoname')
 
 @friday.on(friday_on_cmd(pattern="autobio(?: |$)(.*)"))
@@ -24,7 +36,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    sed = await edit_or_reply(event, "`Started AutoBio Your Bio Will Be Changed Every 1 Min, According To TimeZone Given. To Terminate This Process Do A Restart`")
+    sed = await edit_or_reply(event, "`Started AutoBio Your Bio Will Be Changed Every 1 Min, According To TimeZone Given. To Terminate This Process Use .stop Cmd`")
     scheduler.add_job(auto_bio, 'interval', args=[event.pattern_match.group(1)], minutes=1, id='autobio')
 
 @friday.on(friday_on_cmd(pattern="stop$"))
@@ -36,9 +48,9 @@ async def _(event):
     try:
         scheduler.shutdown()
     except:
-        await event.edit("Are You Fking Insane?")
+        await event.edit("`Are You Fking Insane?`")
         return
-    await event.edit(f"`Process Terminated.`")  
+    await sed.edit("`All Auto Tools Has Been Terminated`")
     
 scheduler.start()
 
