7b74f2a28172417e020c5df7bdc505403cbf9324
スタークギャング <StarkGangZ@gmail.com>
2021-02-12 17:17:32 +0530

    Update autoname.py

--- fridaybot/modules/autoname.py
+++ fridaybot/modules/autoname.py
@@ -6,59 +6,26 @@ import time
 from telethon.errors import FloodWaitError
 from telethon.tl import functions
 from uniborg.util import edit_or_reply, friday_on_cmd, sudo_cmd
-
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from fridaybot.function.auto_tools import auto_name
 from fridaybot import ALIVE_NAME, CMD_HELP
-
-DEL_TIME_OUT = 60
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "FridayUserbot"
 
 
 @friday.on(friday_on_cmd(pattern="autoname"))  # pylint:disable=E0602
 @friday.on(sudo_cmd(pattern="autoname", allow_sudo=True))
 async def _(event):
-    sed = await edit_or_reply(event, "`Starting AutoName Please Wait`")
     if event.fwd_from:
         return
+    sed = await edit_or_reply(event, "`Started AutoName.`")
+    scheduler = AsyncIOScheduler(
+        executors={
+    'default': AsyncIOExecutor(),
+        }
+    )
+    scheduler.add_job(auto_name, 'interval', args=[event.pattern_match.group(1)], minutes=5)
+    scheduler.start()
 
-    while True:
-
-        DM = time.strftime("%d-%m-%y")
-
-        HM = time.strftime("%H:%M")
-
-        name = f"🕒{HM} ⚡{DEFAULTUSER}⚡ 📅{DM}"
-
-        logger.info(name)
-
-        try:
-
-            await borg(
-                functions.account.UpdateProfileRequest(  # pylint:disable=E0602
-                    first_name=name
-                )
-            )
-
-        except FloodWaitError as ex:
-
-            logger.warning(str(e))
-
-            await asyncio.sleep(ex.seconds)
-
-        # else:
-
-        # logger.info(r.stringify())
-
-        # await borg.send_message(  # pylint:disable=E0602
-
-        #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
-
-        #     "Successfully Changed Profile Name"
-
-        # )
-
-        await asyncio.sleep(DEL_TIME_OUT)
-
-    await sed.edit(f"Auto Name has been started my Master")
+    
 
 
 CMD_HELP.update(
