0ca5c49781572e3b4930dfa9d02ff772666d4339
スタークギャング <StarkGangZ@gmail.com>
2021-02-13 12:29:43 +0530

    Update auto_post_sql.py

--- fridaybot/modules/sql_helper/auto_post_sql.py
+++ fridaybot/modules/sql_helper/auto_post_sql.py
@@ -18,7 +18,7 @@ from fridaybot.modules.sql_helper import BASE, SESSION
 class Post(BASE):
     __tablename__ = "post"
     to_post_chat_id = Column(String(14), primary_key=True)
-    target_chat_id = Column(String(14), primary_key=True)
+    target_chat_id = Column(String(14))
 
     def __init__(self, target_chat_id, to_post_chat_id):
         self.target_chat_id = target_chat_id
@@ -28,31 +28,31 @@ class Post(BASE):
 Post.__table__.create(checkfirst=True)
 
 
-def add_new_post_data_in_db(target_chat_id: str, to_post_chat_id: str):
-    post_adder = Post(str(target_chat_id), str(to_post_chat_id))
+def add_new_post_data_in_db(to_post_chat_id: str, target_chat_id: str):
+    post_adder = Post(str(to_post_chat_id), str(target_chat_id))
     SESSION.add(post_adder)
     SESSION.commit()
 
 
-def get_all_post_data(target_chat_id: str):
+def get_all_post_data(to_post_chat_id: str):
     try:
-        s__ = SESSION.query(Post).get(str(target_chat_id))
+        s__ = SESSION.query(Post).get(str(to_post_chat_id))
         return str(s__.target_chat_id), str(s__.to_post_chat_id)
     finally:
         SESSION.close()
 
 
-def is_post_data_in_db(target_chat_id: str):
+def is_post_data_in_db(to_post_chat_id: str):
     try:
-        s__ = SESSION.query(Post).get(str(target_chat_id))
+        s__ = SESSION.query(Post).get(str(to_post_chat_id))
         if s__:
-            return str(s__.to_post_chat_id)
+            return str(s__.target_chat_id)
     finally:
         SESSION.close()
 
 
-def remove_post_data(target_chat_id):
-    sed = SESSION.query(Post).get(str(target_chat_id))
+def remove_post_data(to_post_chat_id):
+    sed = SESSION.query(Post).get(str(to_post_chat_id))
     if sed:
         SESSION.delete(sed)
         SESSION.commit()
