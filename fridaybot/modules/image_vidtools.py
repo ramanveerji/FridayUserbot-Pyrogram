3bce6061e6c72e269d6cbe01ba2fd1c24b23f71f
スタークギャング <StarkGangZ@gmail.com>
2021-02-13 11:57:16 +0530

    Merge Color Code To Color Converter

--- fridaybot/modules/image_vidtools.py
+++ fridaybot/modules/image_vidtools.py
@@ -18,13 +18,9 @@ from shutil import rmtree
 import cv2
 import cv2 as cv
 import random
-from skimage.filters import threshold_local
-import cv2
-import imutils
 import numpy as np
 from datetime import datetime
-
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 import pytz 
 import asyncio
 import requests
@@ -171,6 +167,36 @@ async def _(event):
     elif IdkWtf is True:
       await kok.edit("**IMAGE-RESULT** \n**NSFW :** `True`")
       return 
+    
+@friday.on(friday_on_cmd(pattern="color (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+    if input_str.startswith("#"):
+        try:
+            usercolor = ImageColor.getrgb(input_str)
+        except Exception as e:
+            await event.edit(str(e))
+            return False
+        else:
+            im = Image.new(mode="RGB", size=(1280, 720), color=usercolor)
+            im.save("@Colour.png", "PNG")
+            input_str = input_str.replace("#", "#COLOR_")
+            await borg.send_file(
+                event.chat_id,
+                "@Colour.png",
+                force_document=False,
+                caption=input_str,
+                reply_to=message_id,
+            )
+            os.remove("@Colour.png")
+            await event.delete()
+    else:
+        await event.edit("Syntax: `.color <color_code>`")
     
 @friday.on(friday_on_cmd(pattern=r"thug"))
 @friday.on(sudo_cmd(pattern=r"thug", allow_sudo=True))
