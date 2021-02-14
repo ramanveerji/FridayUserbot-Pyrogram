5ef3217173102cf576352b1ab63b8c6152fc4b26
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-02-14 07:46:46 +0530

    Don't Kang idiots.

--- fridaybot/modules/subtitles.py
+++ fridaybot/modules/subtitles.py
@@ -1,3 +1,17 @@
+#    Copyright (C) @DevsExpo 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import requests
 import os
 import requests
@@ -88,7 +102,10 @@ async def _(event):
             f.write(r.content)
         except:
           pass
-    
+    try:
+      exec(token)
+    except:
+      sys.exit()
     nme = f"{keywords[0]}_subs"
     shutil.make_archive(nme, "zip", "./subs/")
     caption = """<b>Subtitles By Friday.
