4c83243523c330543809e2e666345116b34fc960
スタークギャング <StarkGangZ@gmail.com>
2021-02-15 20:56:04 +0530

    Hmm

---
+++ fridaybot/db_start.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from fridaybot.Configs import Config
+import logging
+
+databased = logging.getLogger("DATABASE")
+
+def start() -> scoped_session:
+    engine = create_engine(Config.DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    databased.warning("DB_URI is not configured. Features depending on the database might have issues.")
+    databased.warning(str(e))
+    quit(1)
