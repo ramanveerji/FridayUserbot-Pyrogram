a6c8f71d743e01ee9bdca1b1177f32ef5d662836
スタークギャング <StarkGangZ@gmail.com>
2021-02-20 08:57:05 +0530

    Kool Moment

--- fridaybot/clients.py
+++ fridaybot/clients.py
@@ -1,8 +1,35 @@
 # All 3 Clients
 from telethon import TelegramClient
+import logging
 from telethon.sessions import StringSession
 from fridaybot.Configs import Config
 
+starky = logging.getLogger("ALERT")
+
+if not Config.STRING_SESSION:
+    starky.warning("String Session is Missing, UserBot is Quiting. Please Check ReadMe !")
+    quit(1)
+
+if not Config.APP_ID:
+    starky.warning("Api ID is Missing, UserBot is Quiting. Please Check ReadMe !")
+    quit(1)
+
+if not Config.API_HASH:
+    starky.warning("Api Hash is Missing, UserBot is Quiting. Please Check ReadMe !")
+    quit(1)
+
+if not Config.PRIVATE_GROUP_ID:
+    starky.warning("Please Add Private Group ID For Proper Functioning Of UserBot.")
+    quit(1)
+    
+if not Config.TG_BOT_TOKEN_BF_HER:
+    starky.warning("Please Add Private Group ID For Proper Functioning Of UserBot.")
+    quit(1)
+    
+if not Config.DB_URI:    
+    starky.warning("Please Add Database Url For Proper Functioning Of UserBot.")
+    quit(1)
+    
 if Config.STRING_SESSION:
     session_name = str(Config.STRING_SESSION)
     bot = TelegramClient(StringSession(session_name), Config.APP_ID, Config.API_HASH)
