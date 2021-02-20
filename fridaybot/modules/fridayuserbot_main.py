52fac9d0d8a3ef6a48b49e23c523afceeead8a0c
スタークギャング <StarkGangZ@gmail.com>
2021-02-20 09:55:51 +0530

    Update fridayuserbot_main.py

--- fridaybot/modules/fridayuserbot_main.py
+++ fridaybot/modules/fridayuserbot_main.py
@@ -146,9 +146,9 @@ async def _(event):
     bothmm = await tgbot.get_me()
     uptime = get_readable_time((time.time() - Lastupdate))
     end = datetime.now()
-    ms = (end - start)
+    ms = (end - start).microseconds / 1000
     await starkislub.edit(
-        f"**█▀█ █▀█ █▄░█ █▀▀ █ \n█▀▀ █▄█ █░▀█ █▄█ ▄**\n➲ `{round(ms)}` \n➲ `{uptime}` \n➲ {rip} \n➲ @{bothmm.username}"
+        f"**█▀█ █▀█ █▄░█ █▀▀ █ \n█▀▀ █▄█ █░▀█ █▄█ ▄**\n➲ `{round(ms)}ms` \n➲ `{uptime}` \n➲ {rip} \n➲ @{bothmm.username}"
     )
 
 
