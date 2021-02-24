4bccae399fcb0bd7317a71160002e53c82796744
スタークギャング <StarkGangZ@gmail.com>
2021-02-24 15:36:07 +0530

    Google Translator Better

--- fridaybot/tr_engines/engine.py
+++ fridaybot/tr_engines/engine.py
@@ -1,24 +1,23 @@
 import os
 from json import loads, JSONDecodeError
 from fridaybot.Configs import Config
+from googletrans import LANGUAGES
+from google_trans_new import google_translator
 import logging
 
 k = logging.getLogger("TR-ENGINE")
 lang = Config.LANG
 
-if not os.path.exists(f'fridaybot/tr_engines/languages_json/{lang}.devsexpojson'):
-    k.warning("Invalid Language. Using Default Language - English")
-    lang_path = f"fridaybot/tr_engines/languages_json/en.devsexpojson"
-else:
-    lang_path = f"fridaybot/tr_engines/languages_json/{lang}.devsexpojson"
-    
-try:
-    lang_string = loads(open(lang_path, encoding='utf-8').read())
-except JSONDecodeError:
-    lang_string = loads(open(f"fridaybot/tr_engines/languages_json/en.devsexpojson", encoding='utf-8').read())
-    k.warning("Invalid JSON File. Reverting To English As Default Language.")
-    
-def tr_engine(string="?"):
-    hmm = lang_string['STRINGS']
-    final_s = hmm.get(string)
-    return final_s   
+async def tr_engine(event, text, parse_mode='md'):
+    kk = Config.LANG if LANGUAGES.get(Config.LANG) else 'en'
+    if kk == 'en':
+        await event.edit(text, parse_mode=parse_mode)
+    else:
+        try:
+            translator = google_translator()
+            translated = translator.translate(text, lang_tgt=kk)
+            hmm = translated
+        except:
+            hmm = text
+    await event.edit(hmm, parse_mode=parse_mode)        
+    return
