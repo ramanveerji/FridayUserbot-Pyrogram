0a0067e56f8dd45c033bc832d1a545c238396244
スタークギャング <StarkGangZ@gmail.com>
2021-02-25 14:58:23 +0530

    Rewrite : voice.py

--- fridaybot/modules/voice.py
+++ fridaybot/modules/voice.py
@@ -1,82 +1,67 @@
-""" Google Text to Speech
-Available Commands:
-.tts LanguageCode as reply to a message
-.tts LangaugeCode | text to speak"""
+#    Copyright (C) @DevsExpo 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 import os
 import subprocess
 from datetime import datetime
-
+from langdetect import detect
 from gtts import gTTS
-
+import gtts
 from fridaybot import CMD_HELP
 from fridaybot.utils import friday_on_cmd
 
 
-@friday.on(friday_on_cmd(pattern="voice (.*)"))
+@friday.on(friday_on_cmd(pattern="(voice|tts|speech|texttospeech)(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = event.pattern_match.group(1)
+    stime = time.time()
+    input_str = event.pattern_match.group(2)
     start = datetime.now()
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        text = previous_message.message
-        lan = input_str
-    elif "|" in input_str:
-        lan, text = input_str.split("|")
-    else:
-        await event.edit("Invalid Syntax. Module stopping.")
+    if not event.reply_to_msg_id:
+        await event.reply("`Please Reply To Message To Convert To Speech!`")
         return
-    text = text.strip()
-    lan = lan.strip()
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    required_file_name = Config.TMP_DOWNLOAD_DIRECTORY + "voice.ogg"
-    try:
-        # https://github.com/SpEcHiDe/UniBorg/commit/17f8682d5d2df7f3921f50271b5b6722c80f4106
-        tts = gTTS(text, lang=lan)
-        tts.save(required_file_name)
-        command_to_execute = [
-            "ffmpeg",
-            "-i",
-            required_file_name,
-            "-map",
-            "0:a",
-            "-codec:a",
-            "libopus",
-            "-b:a",
-            "100k",
-            "-vbr",
-            "on",
-            required_file_name + ".opus",
-        ]
-        try:
-            t_response = subprocess.check_output(
-                command_to_execute, stderr=subprocess.STDOUT
-            )
-        except (subprocess.CalledProcessError, NameError, FileNotFoundError) as exc:
-            await event.edit(str(exc))
-            # continue sending required_file_name
-        else:
-            os.remove(required_file_name)
-            required_file_name = required_file_name + ".opus"
-        end = datetime.now()
-        ms = (end - start).seconds
-        await borg.send_file(
-            event.chat_id,
-            required_file_name,
-            # caption="Processed {} ({}) in {} seconds!".format(text[0:97], lan, ms),
-            reply_to=event.message.reply_to_msg_id,
-            allow_cache=False,
-            voice_note=True,
-        )
-        os.remove(required_file_name)
-        await event.edit("Processed {} ({}) in {} seconds!".format(text[0:97], lan, ms))
-        await asyncio.sleep(5)
-        await event.delete()
-    except Exception as e:
-        await event.edit(str(e))
+    reply_msg = await event.get_reply_message()
+    if not reply_msg.text:
+        await event.edit("`Please Reply To Text Message To Convert To Speech !`")
+        return
+    if input_str:
+        language = input_str
+    else:
+        language = "en"
+    text = reply_msg.raw_text   
+    kk = gtts.lang.tts_langs()
+    if not kk.get(language):
+        await event.edit("`Unsupported Language!`")
+        return
+    tts = gTTS(text, lang=language)
+    tts.save(f'{kk.get(language)}.ogg')
+    dec = detect(text)
+    etime = time.time()
+    hmm_time = round(etime-stime)
+    owo = f"**TTS** \n**Detected Text Language :** `{dec}` \n**Speech Text :** `{language}` \n**Time Taken :** `{hmm_time}`"
+    await friday.send_file(event.chat_id,
+                          file=f'{kk.get(language)}.ogg',
+                          caption=owo,
+                          force_document=False,
+                          allow_cache=False,
+                          reply_to=reply_msg.id
+       )
+    await event.delete()
+    os.remove(f'{kk.get(language)}.ogg')
+        
+        
 
 
 CMD_HELP.update(
