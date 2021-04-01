cd9fdd6cdecf3b737119e42abf63abc91071036e
スタークギャング <StarkGangZ@gmail.com>
2021-04-01 20:15:10 +0530

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -10,6 +10,7 @@ from main_startup.config_var import Config
 from main_startup.core.decorators import friday_on_cmd, listen
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from os import environ, execle, path, remove
+import sys
 from git import Repo
 from datetime import datetime
 import heroku3
