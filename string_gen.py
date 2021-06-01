fd7766a957eefe9f3b134b50049d19f875965b0b
スタークギャング <StarkGangZ@gmail.com>
2021-06-01 13:22:23 +0530

    Update string_gen.py

--- string_gen.py
+++ string_gen.py
@@ -14,11 +14,12 @@ All rights reserved.
 """
 
 print(friday_)
+
 api_id = input("Enter Your API ID: \n")
 api_hash = input("Enter Your API HASH : \n")
 
 with Client("FridayUB", api_id=api_id, api_hash=api_hash) as bot_:
     first_name = (bot_.get_me()).first_name
-    string_session_ = f"<b>String Session For {first_name}</b> \n<code>{bot_.export_session_string()}</code>"
+    string_session_ = f"<b><u>String Session For {first_name}</b></u> \n<code>{bot_.export_session_string()}</code>"
     bot_.send_message("me", string_session_, parse_mode="html")
     print(f"String Has Been Sent To Your Saved Message : {first_name}")
