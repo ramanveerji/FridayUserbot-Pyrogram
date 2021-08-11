4b5640540ad7e6c852fb43634121e83c45b86324
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2021-08-12 00:51:37 +0530

    Update nmodedb.py

--- database/nmodedb.py
+++ database/nmodedb.py
@@ -1,24 +1,20 @@
 from database import db_x
 
-nighttime = db_x["NIGHT_TIME"]
+night_time = db_x["NIGHT_TIME"]
 
 
 async def add_night_chat(chat_id):
-    await nighttime.insert_one({"chat_id": chat_id})
+    await night_time.insert_one({"chat_id": chat_id})
 
 
 async def rm_night_chat(chat_id):
-    await nighttime.delete_one({"chat_id": chat_id})
+    await night_time.delete_one({"chat_id": chat_id})
 
 
 async def get_all_night_chats():
-    lol = [ujwal async for ujwal in nighttime.find({})]
-    return lol
+    return [ujwal async for ujwal in night_time.find({})]
 
 
 async def is_night_chat_in_db(chat_id):
-    k = await nighttime.find_one({"chat_id": chat_id})
-    if k:
-        return True
-    else:
-        return False
+    k = await night_time.find_one({"chat_id": chat_id})
+    return bool(k)
