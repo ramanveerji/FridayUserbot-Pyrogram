8fc5c330ada12139632a3f4361a84e60690b84ca
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2021-08-13 19:26:40 +0530

    Update config_var.py

--- main_startup/config_var.py
+++ main_startup/config_var.py
@@ -43,7 +43,7 @@ class Config((object)):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     STRINGSESSION = os.environ.get("STRINGSESSION", None)
     ASSISTANT_START_PIC = os.environ.get(
-        "ASSISTANT_START_PIC", "https://telegra.ph//file/9a33dee01e203c7d4ff1b.jpg"
+        "ASSISTANT_START_PIC", "https://telegra.ph//file/92c1a600394c723db90fc.jpg"
     )
     STRINGSESSION_2 = os.environ.get("STRINGSESSION_2", None)
     STRINGSESSION_3 = os.environ.get("STRINGSESSION_3", None)
