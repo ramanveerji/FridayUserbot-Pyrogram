187d81f406b89e39dea9ecb7e444377608cca483
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2022-04-08 01:51:18 +0530

    Try 1080

--- plugins/yt.py
+++ plugins/yt.py
@@ -65,7 +65,7 @@ def yt_dl(url, client, message, type_):
         }
     else:
         opts = {
-            "format": "bestvideo[height=720]",
+            "format": "bestvideo[height=<1080]+bestaudio",
             "addmetadata": True,
             "key": "FFmpegMetadata",
             "prefer_ffmpeg": True,
