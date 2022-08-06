45645402a75f4a6595a75f2d1612105f0045640b
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2022-08-06 17:04:17 +0530

    Update main_assistant.py

--- assistant/main_assistant.py
+++ assistant/main_assistant.py
@@ -333,8 +333,7 @@ async def writing(client, message):
     wrt = await message.reply("`Jarvis is writing text on page...`")
     text = message.text.split(" ",1)[1]
     if not text:
-
-text = message.reply_to_message.text
+        text = message.reply_to_message.text
     chat_id = int(message.chat.id)
     file_name = f"{message.chat.id}.jpg"
     length = len(text)
